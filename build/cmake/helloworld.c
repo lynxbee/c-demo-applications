f5d374d6440292de0ad45af8d60ac9483dd777d7
LynxBee Developer <social@lynxbee.com>
2015-09-15 16:27:26 +0530

    This shows how to add version to source code
    Signed-off-by: LynxBee Developer <social@lynxbee.com>

--- build/cmake/helloworld.c
+++ build/cmake/helloworld.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "helloConfig.h"
 
 int main(int argc, char **argv) {
 	printf("Hello World from cmake\n");
+
+	printf("Version %d.%d\n", hello_VERSION_MAJOR, hello_VERSION_MINOR);
+
 	return 0;
 }
